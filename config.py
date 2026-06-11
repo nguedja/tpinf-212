252120ea4bd3378490aec59630508443d7433295
nguedja <divan.nguedja@facsciences-uy1.cm>
2026-06-11 20:29:51 +0200

    fix render sqlite issue

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = "projet_examens_l2_2026"
-
-    SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" +
-        os.path.join(BASE_DIR, "database", "examens.db")
-    )
-
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
